b93a3f8595eb68f60a129d62eec212fae705aba8
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-06-20 15:03:22 -0300

    Update ItensDePedidos.java

--- Projetoo/src/br/com/ItensDePedidos.java
+++ Projetoo/src/br/com/ItensDePedidos.java
@@ -4,36 +4,45 @@ public class ItensDePedidos {
 
 	private int quantidade;
 	private Produto produto;
-	
+
 	public int getQuantidade() {
 		return quantidade;
 	}
+
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
+
 	public Produto getProduto() {
 		return produto;
 	}
+
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
+
 	public boolean incrementaQtde() {
-		return true;
-	
+
+		quantidade++;
+		System.out.println("A quantidade do produto é de" + quantidade);
+
+		return incrementaQtde();
+
 	}
-	
-	 public boolean decrementaQtde() {
-		return false;
-		 
+
+	public boolean decrementaQtde() {
+
+		quantidade--;
+		System.out.println("A quantidade do produto é de" + quantidade);
+
+		return decrementaQtde();
 	}
-	
-	 public double calcularSubTotal() {
-		return calcularSubTotal();
-		 
+
+	public double calcularSubTotal() {
+		return quantidade;
+		
+
+
 	}
-	
-	
-	
-	
+
 }
